5e6f38ce1ca32069781e4f9cf4f95fd432356a86
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-01-17 08:30:52 +0000

    [finish]4-5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "fbc1d4228a2740e3b7b96d59e7e71446.vfs.cloud9.us-east-1.amazonaws.com"
 
-　config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :inline
 end
