91b8a46edff4b02007ca7c340a1ed3d168189137
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-01-19 06:35:06 +0000

    [finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
-
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
+  root to: "homes#top"
   get 'homes/about' => 'homes#about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
